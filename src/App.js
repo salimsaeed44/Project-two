02039f4961a1f7706590f0fc919fd13dfbcf6ea6
SalemSaeed <seedsalim44@gmail.com>
2022-06-27 11:20:40 +0300

    Created by React.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Price from './Price';
+
+function App() {
+  return (
+    <div className="App">
+      <Price />
+    </div>
+  );
+}
+
+export default App;
